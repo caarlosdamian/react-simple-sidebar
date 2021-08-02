4b46bbf2fbae85718042538f6817c5826394d419
caarlosdamian <dapecarlos@gmail.com>
2021-08-01 22:56:52 -0500

    added 3 routes and functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Reports from './pages/Reports'
+
 function App() {
   return (
     <>
       <Router>
         <Sidebar/>
         <Switch>
-          <Route path="/" />
+          <Route path="/"  exact component={Home}/>
+          <Route path="/products" exact component={Products} />
+          <Route path="/reports" exact component={Reports} />
         </Switch>
       </Router>
     </>
